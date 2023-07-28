3755b67349a64b5f4a7f91f05d92526a8fde1ef7
Antonio Iturriaga <91393576+AntonioCait@users.noreply.github.com>
2023-07-28 10:55:28 -0600

    new build
    space y changed, and icon size

--- src/main.ts
+++ src/main.ts
@@ -137,7 +137,7 @@ const filterPartners = () => {
 			key-${index}
 			">
         <div class="partnerInfo flex flex-row justify-between gap-5">
-          <div class="left-box flex flex-col space-y-3">
+          <div class="left-box flex flex-col">
 					  ${
 							partner.isOffice
 								? `<div class='w-28'>
@@ -169,7 +169,7 @@ const filterPartners = () => {
 							</div>`
 								: ''
 						}
-            <p class='font-bold text-md text-ms-text-black mb-1'>${
+            <p class='font-bold text-md text-ms-text-black mb-2'>${
 							// if isOffice is true and equal to Torreón display Torreón
 							// partner.isOffice  ? 'Oficina Regional' : partner.inPageName
 							partner.isOffice ? '' : partner.inPageName.toUpperCase()
@@ -179,9 +179,9 @@ const filterPartners = () => {
 						<!--
             <p class='text-sm text-gray-500'>${partner.certificationName}</p>
 						-->
-						<div class='flex items-center'>
+						<div class='flex items-center mb-1 '>
 							<div class='mr-1'>
-								<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-5" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-6" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
 									<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
 								</svg>
 							</div>
@@ -190,11 +190,11 @@ const filterPartners = () => {
 							</div>
 						</div>
 
-						<div class='flex items-center'>
+						<div class='flex items-center mb-6'>
 							<div class='mr-1'>
 								<!--link to phone clickable on icon-->
 								<a href="tel:${partner.phone}">
-									<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-5" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+									<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-6" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
 										<path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
 									</svg>
 								</a>
@@ -206,7 +206,7 @@ const filterPartners = () => {
 							</div>
 						</div>
 
-						<div class='social-icons flex items-center pt-2'>
+						<div class='social-icons flex items-center'>
 							${
 								// if partner has email, display email icon
 								partner.email
